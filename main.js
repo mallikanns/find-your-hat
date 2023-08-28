b04e843098b2f1bf7982ae481e0d0d9ddd57cbe1
mallikanns <137501517+mallikanns@users.noreply.github.com>
2023-08-28 10:43:10 +0700

    Set up working directory and elements

---
+++ main.js
@@ -0,0 +1,15 @@
+// JavaScript: Find your hat //
+// <GitHub Repo: >
+
+//Thinking process
+// Step 1 : Set up working directory and elements
+
+// Step 1 : Set up working directory and elements
+const prompt = require('prompt-sync')({ sigint: true }); // This sends a SIGINT, or “signal interrupt” message indicating that a user wants to exit a program by press Crtl+c
+const clear = require('clear-screen');//every turn clear the screen that meant you will not get new field in time you choose the direction
+const hat = "⭐";
+const hole = "🌳";
+const fieldCharacter = "⬛";
+const pathCharacter = "👻";
+
+
